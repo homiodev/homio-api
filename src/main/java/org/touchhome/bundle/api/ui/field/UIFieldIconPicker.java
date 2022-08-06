51577c56330a6ec1c93c21ef7536ef20a804e241
CasperDeduct <ruslan.masuk@gmail.com>
2022-08-06 23:36:27 +0300

    inmemory

--- src/main/java/org/touchhome/bundle/api/ui/field/UIFieldIconPicker.java
+++ src/main/java/org/touchhome/bundle/api/ui/field/UIFieldIconPicker.java
@@ -5,10 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for field/method with return type as 'String' to handle field as 'icon' type
+ */
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UIFieldIconPicker {
+    /**
+     * Allow user to select no icon to hide icon at all
+     */
     boolean allowEmptyIcon() default false;
 
+    /**
+     * Allow user to use threshold functionality for showing different icon depend on 'value'
+     */
     boolean allowThreshold() default false;
 }
