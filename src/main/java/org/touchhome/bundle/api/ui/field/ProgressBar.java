847ebd0d860bbc82b4099d25335aca864206855b
CasperDeduct <ruslan.masuk@gmail.com>
2023-03-19 20:49:28 +0200

    Progress bar extens BiConsumer

--- src/main/java/org/touchhome/bundle/api/ui/field/ProgressBar.java
+++ src/main/java/org/touchhome/bundle/api/ui/field/ProgressBar.java
@@ -1,9 +1,16 @@
 package org.touchhome.bundle.api.ui.field;
 
-public interface ProgressBar {
+import java.util.function.BiConsumer;
+
+public interface ProgressBar extends BiConsumer<Double, String> {
     void progress(double progress, String message);
 
     default void done() {
         progress(100, null);
+    }
+
+    @Override
+    default void accept(Double progress, String message) {
+        progress(progress,message);
     }
 }
